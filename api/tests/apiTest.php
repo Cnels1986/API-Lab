85b1506e123a9325db537d5e7aae761238f2e88a
Cnels1986 <CNels1986@gmail.com>
2018-09-02 15:37:43 -0400

    got tests working for finding a game by its id and also the test for updating the games data

--- api/tests/apiTest.php
+++ api/tests/apiTest.php
@@ -73,4 +73,59 @@
       $this->assertSame(200, $response->getStatusCode());
       $this->assertSame($resultString, (string)$response->getBody());
     }
+
+    // Test to get a single game from the table
+    public function testGetGame() {
+
+      // test successful request
+      $resultString = '{"id":"1","name":"Goldeneye 64","year":"1997","console":"Nintendo 64"}';
+
+      $query = $this->createMock('mockQuery');
+      $query->method('fetch')->willReturn(json_decode($resultString, true));
+      $this->db->method('query')->willReturn($query);
+      $env = Environment::mock([
+          'REQUEST_METHOD' => 'GET',
+          'REQUEST_URI'    => '/games/1',
+          ]);
+      $req = Request::createFromEnvironment($env);
+      $this->app->getContainer()['request'] = $req;
+
+      // actually run the request through the app.
+      $response = $this->app->run(true);
+      // assert expected status code and body
+      $this->assertSame(200, $response->getStatusCode());
+      $this->assertSame($resultString, (string)$response->getBody());
+    }
+
+    // This test will test whether or not we can update the information for a game within the table
+    public function testUpdateGame() {
+      // expected result string
+      $resultString = '{"id":"1","name":"Chrono Cross","year":"1999","console":"Playstation 1"}';
+
+      // mock the query class & fetchAll functions
+      $query = $this->createMock('mockQuery');
+      $query->method('fetch')
+        ->willReturn(json_decode($resultString, true)
+      );
+      $this->db->method('query')
+          ->willReturn($query);
+      $this->db->method('exec')
+          ->willReturn(true);
+
+      // mock the request environment.  (part of slim)
+      $env = Environment::mock([
+          'REQUEST_METHOD' => 'PUT',
+          'REQUEST_URI'    => '/games/1',
+          ]);
+      $req = Request::createFromEnvironment($env);
+      $requestBody = ["name" => "Chrono Cross", "year" => "1999", "console" => "Playstation 1"];
+      $req =  $req->withParsedBody($requestBody);
+      $this->app->getContainer()['request'] = $req;
+
+      // actually run the request through the app.
+      $response = $this->app->run(true);
+      // assert expected status code and body
+      $this->assertSame(200, $response->getStatusCode());
+      $this->assertSame($resultString, (string)$response->getBody());
+    }
   }
